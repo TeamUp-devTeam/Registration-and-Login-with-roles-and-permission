904bff50715d5700f90beee051cd59a42da8843e
TeamUp-devTeam <solutions.fortech.problems@gmail.com>
2025-07-29 06:18:42 -0700

    Add files via upload

---
+++ Kan-Tech_Team-Up/src/main/java/com/example/Kan_Tech_Team_Up/KanTechTeamUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.Kan_Tech_Team_Up;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanTechTeamUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanTechTeamUpApplication.class, args);
+	}
+
+}
---
+++ Kan-Tech_Team-Up/src/test/java/com/example/Kan_Tech_Team_Up/KanTechTeamUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Kan_Tech_Team_Up;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanTechTeamUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
